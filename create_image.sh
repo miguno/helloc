526da2379043c3b6a9c7b1f130f9b1ea8e01db03
Michael G. Noll <miguno@miguno.com>
2023-06-06 14:27:20 +0200

    Derive Docker image tag from project version defined in CMakeLists.txt

--- create_image.sh
+++ create_image.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2155
 
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 # `-u`: Errors if a variable is referenced before being set
@@ -7,6 +8,15 @@ set -uo pipefail
 
 # Import environment variables from .env
 set -o allexport && source .env && set +o allexport
+
+CMAKELISTS="CMakeLists.txt"
+declare -r HELLOC_MAJOR_VERSION=$(grep -m 1 HELLOC_MAJOR_VERSION ${CMAKELISTS} | sed -rn 's/^(.*) (.*)\)/\2/p')
+declare -r HELLOC_MINOR_VERSION=$(grep -m 1 HELLOC_MINOR_VERSION ${CMAKELISTS} | sed -rn 's/^(.*) (.*)\)/\2/p')
+declare -r HELLOC_PATCH_VERSION=$(grep -m 1 HELLOC_PATCH_VERSION ${CMAKELISTS} | sed -rn 's/^(.*) (.*)\)/\2/p')
+declare -r HELLOC_DEV_ITERATION=$(grep -m 1 HELLOC_DEV_ITERATION ${CMAKELISTS} | sed -rn 's/^(.*) (.*)\)/\2/p')
+# IMPORTANT: Versioning logic must match with the logic in CMakeLists.txt!
+declare -r HELLOC_VERSION="v${HELLOC_MAJOR_VERSION}.${HELLOC_MINOR_VERSION}.${HELLOC_PATCH_VERSION}-${HELLOC_DEV_ITERATION}"
+DOCKER_IMAGE_TAG="${HELLOC_VERSION}"
 
 echo "Building image '$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG'..."
 # TIP: Add `--progress=plain` to see the full docker output when you are
