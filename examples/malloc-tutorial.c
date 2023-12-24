fa586f99a952043c20c89df8db4aea1438cfdf47
Michael G. Noll <miguno@miguno.com>
2023-12-24 11:42:41 +0100

    Explicitly include stddef.h for size_t

--- examples/malloc-tutorial.c
+++ examples/malloc-tutorial.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <errno.h>
 #include <limits.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- examples/uppercase.c
+++ examples/uppercase.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
